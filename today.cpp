7aa3e1e5f9b8f0b1fd1a526dcefd20bece366ad6
vikash1807 <86599432+vikash1807@users.noreply.github.com>
2021-09-16 20:27:00 +0530

    Add files via upload

---
+++ today.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "size of int is : " << sizeof(int) << endl;
+    cout << "size of float is : " << sizeof(float) << endl;
+    cout << "size of double is : " << sizeof(double) << endl;
+    cout << "size of char is : " << sizeof(char) << endl;
+    return 0;
+}
+
+
+
+//2nd question
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    float x,y;
+    cout << "enter value of 1st number : ";
+    cin >> x;
+    cout << "enter value of 2nd number : ";
+    cin >> y;
+    cout << "value of product of both numbers : " << x*y;
+    return 0;
+}
